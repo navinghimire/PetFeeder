f11459e993736b48bc96f9b7f4abaff870f3c5bc
Navin Ghimire <ghimire.navin@gmail.com>
2017-06-23 15:31:32 -0400

    working feed count

--- countFeed.py
+++ countFeed.py
@@ -1,30 +1,3 @@
-import cv2
-import numpy as np
-
-cap = cv2.VideoCapture(3)
-
-while(1):
-
-	# Take each frame
-	#frame=cv2.imread('testimage1.jpg')
-	_, frame = cap.read()
-	frame = frame[90:380,0:480]
-	# Convert BGR to HSV
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-	# define range of blue color in HSV
-	lower_blue = np.array([0,136,48])
-	upper_blue = np.array([81,234,243])
-
-	# Threshold the HSV image to get only blue colors
-	mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-	# Bitwise-AND mask and original image
-	res = cv2.bitwise_and(frame,frame, mask= mask)
-
-	cv2.imshow('frame',frame)
-	cv2.imshow('mask',mask)
-	cv2.imshow('res',res)
 	k = cv2.waitKey(5) & 0xFF
 	if k == 27:
 		break
--- countFeed_edgeDetection.py
+++ countFeed_edgeDetection.py
@@ -1,87 +1,59 @@
 #!/usr/bin/python
-#Standard imports
 import cv2
 import numpy as np;
 
+cam = cv2.VideoCapture(4)
 
-cam = cv2.VideoCapture(3)
+BLOB_SIZE_MIN = 2000
+BLOB_SIZE_MAX = 4000
+while (True):
+		_,im = cam.read()
+		
+		# Crop a Region 
+		im = im[90:380,0:480]
+		imw = im
 
-_,im = cam.read()
-im = im[90:380,0:480]
-imw = im
-#Blur image
-blur = cv2.medianBlur(im,15)
+		# Blur image
+		blur = cv2.medianBlur(im,15)
 
-#convert color to hsv
-im = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
+		# Convert color to hsv
+		im = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
 
-#upper and lower limits of color to be tracked. In this case brown
-lower_values = np.array([0,0,59])
-upper_values = np.array([107,141,239])
+		# Upper and lower limits of color to be tracked. In this case brown
+		lower_values = np.array([0,0,59])
+		upper_values = np.array([107,141,239])
 
-both = cv2.inRange(im,lower_values,upper_values)
-#cv2.imshow('range',both)
+		both = cv2.inRange(im,lower_values,upper_values)
+		#cv2.imshow('range',both)
 
-# erod the image
-erode = cv2.erode(both,None,iterations = 2)
-#cv2.imshow('eroded',erode)
-#dilate image
-dilate = cv2.dilate(erode, None, iterations = 5)
-#cv2.imshow('dilated',dilate)
-contours,im2 = cv2.findContours(dilate,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-drawing = np.zeros([100,100],np.uint8)
-cv2.drawContours(imw,contours,-1,(0,255,0),3)
-cv2.imshow('contours',imw)
-print cv2.contourArea(contours[0])
-print cv2.contourArea(contours[1])
-print str(len(contours))
-total = 0
-for cnt in contours:
-	print cv2.contourArea(cnt)
-	if cv2.contourArea(cnt) > 1000:
-		total = total + 1	
-print "Total blobs detected: " + str(total)
-# Setup SimpleBlobDetector parameters.
-params = cv2.SimpleBlobDetector_Params()
+		# Erod the image
+		erode = cv2.erode(both,None,iterations = 2)
+		#cv2.imshow('eroded',erode)
 
-# Change thresholds
-params.minThreshold = 10
-params.maxThreshold = 200
+		# Dilate image
+		dilate = cv2.dilate(erode, None, iterations = 5)
+		#cv2.imshow('dilated',dilate)
+		
+		# Get Contours
+		contours,im2 = cv2.findContours(dilate,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
+		cv2.drawContours(imw,contours,-1,(0,255,0),3)
 
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 1500
+		total = 0
+		for cnt in contours:
+			if cv2.contourArea(cnt) > BLOB_SIZE_MAX :
+				# Area divided by the approximate size of the blob
+				total = total + (int(cv2.contourArea(cnt))/BLOB_SIZE_MAX)
+			elif cv2.contourArea(cnt) > BLOB_SIZE_MIN:
+				total = total + 1	
+			else:
+				total = total
+					
+		cv2.imshow('contours',imw)
+		print total 
+		k = cv2.waitKey(5) & 0xFF 
+		if k == 27: 
+				break 
+ 
+cv2.destroyAllWindows() 
 
-# Filter by Circularity
-params.filterByCircularity = True
-params.minCircularity = 0.1
-
-# Filter by Convexity
-params.filterByConvexity = True
-params.minConvexity = 0.87
-    
-# Filter by Inertia
-params.filterByInertia = True
-params.minInertiaRatio = 0.01
-
-# Create a detector with the parameters
-ver = (cv2.__version__).split('.')
-if int(ver[0]) < 3 :
-	detector = cv2.SimpleBlobDetector(params)
-else : 
-	detector = cv2.SimpleBlobDetector_create(params)
-
-
-# Detect blobs.
-keypoints = detector.detect(im)
-
-# Draw detected blobs as red circles.
-# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-# the size of the circle corresponds to the size of blob
-
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-# Show blobs
-cv2.imshow("Keypoints", im_with_keypoints)
-cv2.waitKey(0)
